8a42750f338e26a56ced2e3cf2888140d2c225c2
JonasPalms <jonas.p.sorensen@gmail.com>
2022-01-13 10:52:50 +0100

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,102 @@
-import logo from './logo.svg';
+
+import { useEffect, useState } from 'react';
 import './App.css';
+import SingleCard from './components/SingleCard';
+
+
+const cardImages = [
+  { "src": "/img/2646.jpeg", matched: false },
+  { "src": "/img/2651.jpeg", matched: false },
+  { "src": "/img/2652.jpeg", matched: false },
+  { "src": "/img/2657.jpeg", matched: false },
+  { "src": "/img/2658.jpeg", matched: false },
+  { "src": "/img/2665.jpeg", matched: false },
+]
 
 function App() {
+
+  const [cards, setCards] = useState([])
+  const [turns, setTurns] = useState([0])
+  const [choiceOne, setChoiceOne] = useState(null)
+  const [choiceTwo, setChoiceTwo] = useState(null)
+  const [disabled, setDisbabled] = useState(false)
+
+  // shuffle cards 
+  const shuffleCards = () => {
+    const shuffledCards = [...cardImages, ...cardImages]
+      .sort(() => Math.random() - 0.5)
+      .map(card => ({ ...card, id: Math.random() }))
+    // this shuffling and ID assignment is not optimal but will do for now eh 
+    setChoiceTwo(null)
+    setChoiceTwo(null)
+    setCards(shuffledCards)
+    setTurns(0)
+  }
+
+  // handle a choice 
+
+  const handleChoice = (card) => {
+    choiceOne ? setChoiceTwo(card) : setChoiceOne(card)
+
+  }
+
+  // compare two selected cards 
+
+  useEffect(() => {
+
+    if (choiceOne && choiceTwo) {
+      setDisbabled(true)
+      if (choiceOne.src === choiceTwo.src) {
+        setCards(prevCards => {
+          return prevCards.map(card => {
+            if (card.src === choiceOne.src) {
+              return { ...card, matched: true }
+            } else {
+              return card
+            }
+          })
+        })
+        resetTurn()
+      } else {
+
+        setTimeout(() => resetTurn(), 1000)
+      }
+    }
+  }, [choiceOne, choiceTwo])
+
+  console.log(cards)
+
+  // reset choices and increase turn
+  const resetTurn = () => {
+    setChoiceOne(null)
+    setChoiceTwo(null)
+    setTurns(prevTurns => prevTurns + 1)
+    setDisbabled(false)
+  }
+
+  // start a new game automatically
+  useEffect(() => {
+    shuffleCards()
+  }, [])
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>VEND EN LEGENDE</h1>
+      <button onClick={shuffleCards}>Nyt spil</button>
+      <div className="card-grid">
+        {cards.map(card => (
+          <SingleCard
+            key={card.id}
+            card={card}
+            handleChoice={handleChoice}
+            flipped={
+              card === choiceOne || card === choiceTwo || card.matched}
+            disabled={disabled}
+          />
+        ))}
+      </div>
+      <p className="turns"> forsøg: {turns}</p>
+      <p className='copyright'>Copyright Dansk Boldspil-Union &copy;</p>
     </div>
   );
 }
